bd8ffe941969dabdede475f353f77fc495f40f2b
Gleb Kochubey <hlib.kochubei@gmail.com>
2022-12-01 17:23:59 +0100

    Add 'index' method for Task Controller

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,9 @@
+def index
+  tasks = Task.all
+              .ransack(ransack_params)
+              .result
+              .page(page)
+              .per(per_page)
+
+  respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+end
